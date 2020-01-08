aefcd0dae9c532873d76462a2842dfc4890a4020
sdabhi23 <shrey.dabhi23@gmail.com>
2020-01-08 11:38:09 +0530

    refactor code

--- E2.py
+++ E2.py
@@ -1,83 +1,124 @@
 import numpy as np
 from functools import reduce
 
+
 # Calculate the norm square of the jth element of the ith eigenvector using the alternate algorithm
 # H is a square np.matrix
-# i,j are integers between 0 and n-1, inclusive
-def vijsq(H, i, j):
-	# Get the size of the matrix
-	n = H.shape[0]
-	# Get the eigenvalues of the matrix using numpy
-	eigenvalues = np.linalg.eigvalsh(H)
-	# Determine the jth minor
-	Mj = np.delete(np.delete(H, j, 0), j, 1)
-	# Get the eigenvalues of the minor
-	minor_eigenvalues = np.linalg.eigvalsh(Mj)
-	# Calculate the numerator
-	numerator = 1.
-	for k in range(n - 1):
-		numerator *= eigenvalues[i] - minor_eigenvalues[k]
-	# Calculate the denominator
-	denominator = 1.
-	for k in range(n):
-		if k == i: continue
-		denominator *= eigenvalues[i] - eigenvalues[k]
-	return numerator / denominator
-#Calculate the norm square with performance improvements
-def vijsqperf(H, i, j):
-	# Get the size of the matrix
-	n = H.shape[0]
-	# Get the eigenvalues of the matrix using numpy
-	eigenvalues = np.linalg.eigvalsh(H)
-	# Determine the jth minor
-	Mj = np.delete(np.delete(H, j, 0), j, 1)
-	# Get the eigenvalues of the minor
-	minor_eigenvalues = np.linalg.eigvalsh(Mj)
-	# Calculate the numerator
-	eigenvaluei = eigenvalues[i]
-	numerator = reduce( lambda x, y: x*y, [eigenvaluei - minor_eigenvalues[k] for k in range(n-1)])
-	# Calculate the denominator
-	denominator = reduce( lambda x, y: x*y, [eigenvaluei - eigenvalues[k] for k in range(n) if k!= i])
-	return numerator / denominator
+def vij_sq(H, i, j):
+    # Get the size of the matrix
+    n = H.shape[0]
+
+    # Get the eigenvalues of the matrix using numpy
+    eigenvalues = np.linalg.eigvalsh(H)
+
+    # Determine the jth minor
+    Mj = np.delete(np.delete(H, j, 0), j, 1)
+
+    # Get the eigenvalues of the minor
+    minor_eigenvalues = np.linalg.eigvalsh(Mj)
+
+    # Calculate the numerator
+    numerator = 1.
+    for k in range(n - 1):
+        numerator *= eigenvalues[i] - minor_eigenvalues[k]
+
+    # Calculate the denominator
+    denominator = 1.
+    for k in range(n):
+        if k == i:
+            continue
+        denominator *= eigenvalues[i] - eigenvalues[k]
+
+    return numerator / denominator
 
 
-# Calculate the norm square of the jth element of the ith eigenvector using numpy's built in eigenvector calculator
+def eig_sq(H):
+    # Get the size of the matrix
+    n = H.shape[0]
+    res = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(n):
+            res[i, j] = vij_sq(H, j, i)
+
+    return res
+
+
+# Calculate the norm square with performance improvements
+def vij_sq_perf(H, i, j):
+    # Get the size of the matrix
+    n = H.shape[0]
+
+    # Get the eigenvalues of the matrix using numpy
+    eigenvalues = np.linalg.eigvalsh(H)
+
+    # Determine the jth minor
+    Mj = np.delete(np.delete(H, j, 0), j, 1)
+
+    # Get the eigenvalues of the minor
+    minor_eigenvalues = np.linalg.eigvalsh(Mj)
+
+    # Calculate the numerator
+    eigenvaluei = eigenvalues[i]
+    numerator = reduce(lambda x, y: x * y, [eigenvaluei - minor_eigenvalues[k] for k in range(n - 1)])
+
+    # Calculate the denominator
+    denominator = reduce(lambda x, y: x * y, [eigenvaluei - eigenvalues[k] for k in range(n) if k != i])
+
+    return numerator / denominator
+
+
+def eig_sq_perf(H):
+    # Get the size of the matrix
+    n = H.shape[0]
+    res = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(n):
+            res[i, j] = vij_sq_perf(H, j, i)
+
+    return res
+
+
+# Calculate the norm square of all the elements of all the eigenvectors using numpy's built in eigenvector calculator
 # H is a square np.matrix
-# i,j are integers between 0 and n-1, inclusive
-def vijsq_np(H, i, j):
-	# Get the eigenvalues and eigenvalues of the matrix
-	eigenvalues, eigenvectors = np.linalg.eigh(H)
-	return abs(eigenvectors[j, i]) ** 2
+def eig_sq_np(H):
+    # Get the eigenvalues and eigenvalues of the matrix
+    eigenvalues, eigenvectors = np.linalg.eigh(H)
+    return abs(eigenvectors) ** 2
+
 
 # Prints true for each element that agrees between the E2 method and the method built into numpy
 # H is a square np.matrix
 # mode determines comparison with normal or improved function
-def compare(H, mode = 0):
-	# Get the size of the matrix
-	n = H.shape[0]
-	# The tolerance for comparison purposes
-	tol = 1e-12
-	# The matrix that will be returned
-	res = np.empty((n, n), dtype = np.bool)
-	for i in range(n):
-		for j in range(n):
-			# Fill in the matrix with True or False depending on if they agree or not
-			if mode != 0:
-				res[i, j] = abs(vijsqperf(H, i, j) - vijsq_np(H, i, j)) < tol
-			else:
-				res[i, j] = abs(vijsq(H, i, j) - vijsq_np(H, i, j)) < tol
-	return res
+def compare(H, mode=0):
+    # The tolerance for comparison purposes
+    tol = 1e-12
+
+    # Select function depending upon the value of mode
+    if mode == 0:
+        res = eig_sq(H)
+    elif mode == 1:
+        res = eig_sq_perf(H)
+
+    # Return the comparison matrix
+    return abs(res - eig_sq_np(H)) < tol
+
 
 if __name__ == "__main__":
-	# Generate a random Hermitian matrix
-	# Dimension of the matrix
-	n = 8
-	H = np.random.rand(n, n) + 1j * np.random.rand(n, n)
-	H = np.matrix(H)
-	H = 0.5 * (H + H.H)
+    # Generate a random Hermitian matrix
+    # Dimension of the matrix
+    n = 8
+    H = np.random.rand(n, n) + 1j * np.random.rand(n, n)
+    H = 0.5 * (H + H.T)
 
-	# Print the second element of the first eigenvector as calculated in both methods
-	print(vijsq(H, 0, 1))
-	print(vijsq_np(H, 0, 1))
-	# Print the comparison of the two methods, should be a matrix full of Trues
-	print(compare(H))
+    # Print the second element of the first eigenvector as calculated in both methods
+    print("\n--- using the algorithm ---")
+    basic = eig_sq(H)
+    print("basic implementation: ", basic[0, 1])
+    perf = eig_sq_perf(H)
+    print("performant implementation: ", perf[0, 1])
+
+    print("\n--- using numpy ---")
+    nump = eig_sq_np(H)
+    print(nump[0, 1])
